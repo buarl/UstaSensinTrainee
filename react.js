9fb1e55e0ae6a24cb5d66c58b7f2677c89a29100
buarl <114435618+buarl@users.noreply.github.com>
2022-09-26 22:14:04 +0300

    Add files via upload

---
+++ react.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'React'
+
+export default class react extends Component {
+    render() { //The render method returns a description of what you want to see on the screen. React takes the description and displays the result. In particular, render returns a React element, which is a lightweight description of what to render.
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
