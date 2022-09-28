17d41e2f64a4552f4259edc77e3c58fbc01e63e1
buarl <114435618+buarl@users.noreply.github.com>
2022-09-28 21:31:34 +0300

    Update CategoryList.js

--- CategoryList.js
+++ CategoryList.js
@@ -1,11 +1,33 @@
-import React, {Component } from 'react'
+import React, {Component } from 'react';
+import {ListGroup, ListGroupItem} from 'reactstrap';
 
 export default class CategoryList extends Component {
+    state = {categories: [{categoryID:1, categoryName:"El Aletleri"},
+                          {categoryID:2, categoryName:"Elektronik"}],
+
+      currentCategory:""
+  };
+  changeCategory = (category)=>{
+    this.setState({currentCategory:category.categoryName});
+  };
     render() {
         return (
             <div>
-                <h3> Category List</h3>
+                <h3> {this.props.info.title} </h3>
+                <ListGroup>
+                    {
+                        this.state.categories.map(category=>( /*map = döngü fonksiyonu*/
+                            <ListGroupItem onClick = {()=>this.changeCategory(category)} 
+                                                     key= {category.categoryID}> {category.categoryName} 
+                            </ListGroupItem>
+                        ))
+                    }
+                  <ListGroupItem> Mekanik </ListGroupItem>
+                  <ListGroupItem> El Aletleri </ListGroupItem>
+                  <ListGroupItem> Hirdavat </ListGroupItem>
+                </ListGroup>
+                <h4>{this.state.currentCategory}</h4>
             </div>
-        )
+        );
     }
 }
