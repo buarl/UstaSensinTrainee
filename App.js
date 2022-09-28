92d1fe2183261c7b6a321e18110874dbdc53a241
buarl <114435618+buarl@users.noreply.github.com>
2022-09-28 22:13:06 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Navi from './Navi';
+import CategoryList from './CategoryList';
+import ProductList from './ProductList';
+import {Container, Row, Col} from 'reactstrap';
+
+export default class App extends Component {
+   state={currentCategory:""}
+   changeCategory = (category)=>{
+      this.setState({currentCategory:category.categoryName});
+    };
+    render(){
+      let productInfo = {title:"Product List"};
+      let categoryInfo = {title:"Category List"};
+
+      return (
+         <div>
+            <Container>
+                <Row>
+                   <Navi/>
+                </Row>
+                <Row>
+                   <Col xs="3">
+                     <CategoryList currentCategory={this.state.currentCategory} changeCategory = {this.changeCategory}info = {categoryInfo}/>  
+                   </Col>
+                   <Col xs="9">
+                     <ProductList info = {productInfo}/> 
+                   </Col>
+                </Row>
+            </Container>
+         </div>
+    
+       );
+    }
+}
