543486f5111986f2ad42b2e83aca2356decae555
buarl <114435618+buarl@users.noreply.github.com>
2022-10-18 18:18:26 +0300

    Update App.js

--- App.js
+++ App.js
@@ -20,13 +20,13 @@ export default class App extends Component {
                    <Navi/>
                 </Row>
                 <Row>
-                   <Col xs="3">
+                   <Col xs="3"> //bootstrap 12 eşit parçaya ayırıyor, 3'ünü kullandım.
                      <CategoryList 
                      currentCategory={this.state.currentCategory} //state: bir component'e ait özel data
                      changeCategory = {this.changeCategory}
                      info = {categoryInfo}/>  
                    </Col>
-                   <Col xs="9">
+                   <Col xs="9"> //geriye kalan 9 parçayı buraya ayırdım.
                      <ProductList 
                      products = {this.state.products}
                      currentCategory = {this.state.currentCategory}
